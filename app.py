13d93bb03c5b9dd5a67144851f30150cdb0710c2
Abhinandan Sahgal <abhi@Abhinandans-MacBook-Pro.local>
2024-10-16 00:10:23 +0530

    add

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def handle_message(message):
     send(f"Echo: {message}")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=8000)
